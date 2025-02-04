c13bbcf10c9bf3c77323073e17d3f1a4147f4cd4
Mohankanth M <mohan25057@gmail.com>
2025-02-04 10:08:32 +0530

    Create Best Time_to_Buy_and_Sell_Stock.py

---
+++ 04.02.2025/Best Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        min_price = float("inf")
+        max_profit = 0
+
+        for i in prices:
+            if i < min_price:
+                min_price = i
+            elif max_profit < i - min_price:
+                max_profit = i - min_price
+        
+        return max_profit
+        
